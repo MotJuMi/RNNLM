06c8874fe07c6521b3829dbc8c3648e7817abe44
MotJuMi <motjumi@gmail.com>
2018-05-01 01:04:25 +0500

    Edit loss

--- code/model/loss.py
+++ code/model/loss.py
@@ -1,4 +1,9 @@
 import torch.nn.functional as F
 
 def my_loss(pred, target):
+    #print('LOSS:')
+    #print('pred.size(): ' + str(pred.size()))
+    #print('target.size(): ' + str(target.size()))
+    #log_sm = F.log_softmax(pred, dim=1)
+    #print('log_sm.size(): ' + str(log_sm.size()))
     return F.nll_loss(F.log_softmax(pred, dim=1), target)
