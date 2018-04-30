945f229a4a023bf1fe92b27b1aed3e929644617d
MotJuMi <motjumi@gmail.com>
2018-05-01 01:04:39 +0500

    Edit model

--- code/model/model.py
+++ code/model/model.py
@@ -2,34 +2,39 @@ from base import BaseModel
 import torch.nn as nn
 
 class RNNLM(BaseModel):
-    def __init__(self, config, rnn_type='LSTM', ntoken=50000,
-                 ninput=300, nhidden=128, nlayers=2, dropout=0.5,
-                 tie_weights=False):
+    def __init__(self, config):
         super(RNNLM, self).__init__(config)
         self.config = config
-        self.drop = nn.Dropout(dropout)
-        self.encoder = nn.Embedding(ntoken, ninput)
-        if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(ninput, nhidden, nlayers, dropout=dropout)
+        self.rnn_type = self.config['rnn_type']
+        self.ninput = self.config['ninput']
+        self.nhidden = self.config['nhidden']
+        self.nlayers = self.config['nlayers']
+        self.ntoken = self.config['ntoken']
+        self.dropout = self.config['dropout']
+        self.tie_weights = self.config['tie_weights']
+        self.drop = nn.Dropout(self.dropout)
+        self.encoder = nn.Embedding(self.ntoken, self.ninput)
+        if self.rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, self.rnn_type)(self.ninput, self.nhidden, self.nlayers, dropout=self.dropout)
         else:
             try:
-                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+                nonlinearity = {'RNN_TANH': 'tanh', 'RNN_RELU': 'relu'}[self.rnn_type]
             except KeyError:
                 raise ValueError( """An invalid option for `--model` was supplied,
                                  options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""")
-            self.rnn = nn.RNN(ninput, nhidden, nlayers, nonlinearity=nonlinearity, dropout=dropout)
-        self.decoder = nn.Linear(nhidden, ntoken)
+            self.rnn = nn.RNN(self.ninput, self.nhidden, self.nlayers, nonlinearity=nonlinearity, dropout=self.dropout)
+        self.decoder = nn.Linear(self.nhidden, self.ntoken)
 
-        if tie_weights:
-            if nhidden != ninput:
+        if self.tie_weights:
+            if self.nhidden != self.ninput:
                 raise ValueError('When using the tied flag, nhidden must be equal to emsize')
             self.decoder.weight = self.encoder.weight
 
         self.init_weights()
 
-        self.rnn_type = rnn_type
-        self.nhidden = nhidden
-        self.nlayers = nlayers
+        self.rnn_type = self.rnn_type
+        self.nhidden = self.nhidden
+        self.nlayers = self.nlayers
 
     def init_weights(self):
         initrange = 0.1
@@ -38,6 +43,7 @@ class RNNLM(BaseModel):
         self.decoder.weight.data.uniform_(-initrange, initrange)
 
     def forward(self, input, hidden):
+        input = input.t().contiguous()
         emb = self.drop(self.encoder(input))
         output, hidden = self.rnn(emb, hidden)
         output = self.drop(output)
