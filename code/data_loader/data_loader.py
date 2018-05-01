f2c0c8e9a6ee1752cedac628fa368f8ed23b9d3e
MotJuMi <motjumi@gmail.com>
2018-05-02 00:57:29 +0500

    Edit data size in data loader

--- code/data_loader/data_loader.py
+++ code/data_loader/data_loader.py
@@ -3,7 +3,8 @@ import json
 import os
 import torch
 import torch.utils.data as data
-from collections import defaultdict
+from collections import defaultdict, Counter
+import argparse
 
 class Vocabulary(object):
     def __init__(self):
@@ -23,17 +24,20 @@ class Vocabulary(object):
         return len(self.i2w)
 
 class Dataset(data.Dataset):
-    def __init__(self, path, vocab=None, seq_len=20, batch_size=32):
+    def __init__(self, path, vocab=None, seq_len=20, batch_size=32, min_word_count=5):
         self.vocab = Vocabulary() if vocab is None else vocab
-        self.data = self._get_data(path)
+        self.min_word_count = min_word_count
         if vocab is None:
+            self._data_unk(path)
             self.vocab.build_vocab()
+        self.data = self._get_data(path)
         self.batch_size = batch_size
         self.nbatch = len(self.data) // (batch_size * seq_len)
         self.seq_len = seq_len
         #self.data = self.data[:self.nbatch * self.batch_size * self.seq_len + 1]
         self.src_seqs = list(self._get_src_seqs())[:self.nbatch * self.batch_size]
         self.tgt_seqs = list(self._get_tgt_seqs())[:self.nbatch * self.batch_size]
+        self.data = None
         #print('batch_size: ' + str(self.batch_size))
         #print('nbatch: ' + str(self.nbatch))
         #print('seq_len: ' + str(self.seq_len))
@@ -41,9 +45,18 @@ class Dataset(data.Dataset):
         #print('ntgt: ' + str(len(self.tgt_seqs)))
         self.num_total_seqs = len(self.src_seqs)
 
+    def _data_unk(self, path):
+        counter = Counter()
+        with open(path, 'r') as f:
+            ids = []
+            for line in f:
+                tokens = line.strip().split(" ")
+                counter.update(tokens)
+        words = [word for word, count in counter.items() if count >= self.min_word_count]
+        for word in words:
+            self.vocab.add_word(word)
 
     def _get_data(self, path):
-        ids = torch.LongTensor()
         with open(path, 'r') as f:
             ids = []
             for line in f:
@@ -96,9 +109,10 @@ def collate_fn(data):
     #return src_seqs, src_lengths, tgt_seqs, tgt_lengths
     return src_seqs, tgt_seqs
 
-def get_loader(config, mode='train', vocab=None, shuffle=False, seq_len=20, batch_size=32):
+def get_loader(config, mode='train', vocab=None, shuffle=False,
+               seq_len=20, batch_size=32, min_word_count=5):
     path = config['data_loader']['data_dir'] + '/' + mode + '.txt'
-    dataset = Dataset(path, vocab, seq_len, batch_size)
+    dataset = Dataset(path, vocab, seq_len, batch_size, min_word_count)
     data_loader = torch.utils.data.DataLoader(dataset=dataset,
                                               batch_size=batch_size,
                                               shuffle=shuffle,
