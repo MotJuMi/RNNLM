d820cf5a15f4a552e4eeb831eae86568f04f44f7
MotJuMi <motjumi@gmail.com>
2018-05-01 15:23:45 +0500

    Add logger

---
+++ code/logger/logger.py
@@ -0,0 +1,11 @@
+import json
+
+class Logger:
+    def __init__(self):
+        self.entries = {}
+
+    def add_entry(self, entry):
+        self.entries[len(self.entries) + 1] = entry
+
+    def __str__(self):
+        return json.dumps(self.entries, sort_keys=True, indent=4)
